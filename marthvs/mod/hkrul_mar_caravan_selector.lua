06afcf40e34f9dbcb571b893465b92871dc6a73c
RhoxGSAINT <swordknight@naver.com>
2023-11-16 13:05:50 +0900

    scaling update
    scaling update

--- marthvs/mod/hkrul_mar_caravan_selector.lua
+++ marthvs/mod/hkrul_mar_caravan_selector.lua
@@ -3,6 +3,6 @@ return {
     ["hkrul_lector_manann"] = {"hkrul_lector_manann_1","hkrul_lector_manann_2"},
     ["hkrul_slaver"] = {"hkrul_slaver_1","hkrul_slaver_2"},
     ["mar_sea_wizard"] = {"ovn_art_set_mar_sea_wizard_01","ovn_art_set_mar_sea_wizard_02"},
-    ["hkrul_fooger_caravan_master"] = {"hkrul_fooger_caravan_master_01","hkrul_fooger_caravan_master_02"}
+    ["hkrul_fooger_caravan_master"] = {"hkrul_fooger_caravan_master_01","hkrul_fooger_caravan_master_02"},
     ["hkrul_fooger_dwarf_lord"] = {"hkrul_fooger_dwarf_lord_01","hkrul_fooger_dwarf_lord_02"}
 };
