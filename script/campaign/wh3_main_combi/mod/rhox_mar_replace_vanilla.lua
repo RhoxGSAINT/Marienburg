770cfea0a36e12fbc693867cb981b280f4aef083
RhoxGSAINT <swordknight@naver.com>
2023-11-21 20:47:56 +0900

    4.1

--- script/campaign/wh3_main_combi/mod/rhox_mar_replace_vanilla.lua
+++ script/campaign/wh3_main_combi/mod/rhox_mar_replace_vanilla.lua
@@ -168,21 +168,25 @@ function caravans:initialise()
 			if faction:is_human() then
 				self:reward_item_check(faction, region_name, context:caravan_master())
 			end
-			--faction has tech that grants extra trade tariffs bonus after every caravan - create scripted bundle
-			if faction:has_technology("wh3_dlc23_tech_chd_industry_24") then
-				local temp_trade = self.trade_modifier + 5
-				self.trade_modifier = temp_trade
+			
+			-- faction has tech that grants extra trade tariffs bonus after every caravan - create scripted bundle
+			local bv = cm:get_factions_bonus_value(faction, "chd_convoy_trade_tariff_scripted")
+			if bv > 0 then
+				local trade_modifier = cm:get_saved_value("convoy_trade_modifier_" .. faction_key) or 0
+				trade_modifier = trade_modifier + bv
+				cm:set_saved_value("convoy_trade_modifier_" .. faction_key, trade_modifier)
 				local trade_effect = "wh_main_effect_economy_trade_tariff_mod"
-				local trade_effect_bundle = cm:create_new_custom_effect_bundle("wh3_dlc23_effect_chd_convoy_trade_tariff_scripted_bundle")
+				local trade_effect_bundle_key = "wh3_dlc23_effect_chd_convoy_trade_tariff_scripted_bundle"
+				local trade_effect_bundle = cm:create_new_custom_effect_bundle(trade_effect_bundle_key)
 
-				trade_effect_bundle:add_effect(trade_effect, "faction_to_faction_own_unseen", temp_trade)
+				trade_effect_bundle:add_effect(trade_effect, "faction_to_faction_own_unseen", trade_modifier)
 				trade_effect_bundle:set_duration(0)
 
-				if faction:has_effect_bundle(trade_effect_bundle:key()) then
-					cm:remove_effect_bundle(trade_effect_bundle:key(), faction:name())
-				end				
+				if faction:has_effect_bundle(trade_effect_bundle_key) then
+					cm:remove_effect_bundle(trade_effect_bundle_key, faction_key)
+				end
+				
 				cm:apply_custom_effect_bundle_to_faction(trade_effect_bundle, faction)
-
 			end
 				
 			if not region_owner:is_null_interface() then
@@ -190,9 +194,15 @@ function caravans:initialise()
 				cm:cai_insert_caravan_diplomatic_event(region_owner_key,faction_key)
 
 				if region_owner:is_human() and faction_key ~= region_owner_key then
+					local incident_key = "wh3_main_cth_caravan_completed_received"
+					
+					if faction:culture() == "wh3_dlc23_chd_chaos_dwarfs" then
+						incident_key = "wh3_dlc23_chd_convoy_completed_received"
+					end
+					
 					cm:trigger_incident_with_targets(
 						region_owner:command_queue_index(),
-						"wh3_main_cth_caravan_completed_received",
+						incident_key,
 						0,
 						0,
 						0,
